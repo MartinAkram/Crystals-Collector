ee9eec608c58df3325cbadd9376c217f52510143
Martin Akram <martin.akram@gmail.com>
2019-06-07 21:07:34 -0400

    making significant updates to jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,84 @@
 $(document).ready(function() {
+  //Global Variables
+  var wins = 0;
+  var losses = 0;
+  var totalGuessed = 0;
+
   //This variable, "myRandomNumber" generates a random number that the user must match
-  var myRandomNumber = Math.floor(Math.random() * 102) + 19;
-  $("#targetNumber").append(myRandomNumber);
+  function generateRandomNumber() {
+    var myRandomNumber = Math.floor(Math.random() * 102) + 19;
+    $("#targetNumber").append(myRandomNumber);
+  }
+  generateRandomNumber();
+
+  var myRandomNumber = $("#targetNumber").html();
+  console.log(myRandomNumber);
 
   var crystal1 = Math.floor(Math.random() * 12) + 1;
   var crystal2 = Math.floor(Math.random() * 12) + 1;
   var crystal3 = Math.floor(Math.random() * 12) + 1;
   var crystal4 = Math.floor(Math.random() * 12) + 1;
 
-  //   function assignCrystalValue() {
-  var totalGuessed = 0;
-
   $(".crystal").on("click", function() {
     whichCrystal = $(this).attr("value");
-    if (whichCrystal === "crystal-1") {
-      crystalValue1 = crystal1;
-      totalGuessed = totalGuessed + crystalValue1;
-      $("#totalGuessed").html(totalGuessed);
-    } else if (whichCrystal === "crystal-2") {
-      crystalValue2 = crystal2;
-      totalGuessed = totalGuessed + crystalValue2;
-      $("#totalGuessed").html(totalGuessed);
-    } else if (whichCrystal === "crystal-3") {
-      crystalValue3 = crystal3;
-      totalGuessed = totalGuessed + crystalValue3;
-      $("#totalGuessed").html(totalGuessed);
-    } else if (whichCrystal === "crystal-4") {
-      crystalValue4 = crystal4;
-      totalGuessed = totalGuessed + crystalValue4;
-      $("#totalGuessed").html(totalGuessed);
+    if (totalGuessed < myRandomNumber) {
+      if (whichCrystal === "crystal-1") {
+        crystalValue1 = crystal1;
+        totalGuessed = totalGuessed + crystalValue1;
+        $("#totalGuessed").html(totalGuessed);
+        console.log(totalGuessed);
+        if (totalGuessed === myRandomNumber) {
+          console.log("You Win!");
+          wins++;
+          $("#wins").html(wins);
+        } else if (totalGuessed > myRandomNumber) {
+          console.log("You Lost!");
+          losses++;
+          $("#losses").html(losses);
+        }
+      } else if (whichCrystal === "crystal-2") {
+        crystalValue2 = crystal2;
+        totalGuessed = totalGuessed + crystalValue2;
+        $("#totalGuessed").html(totalGuessed);
+        console.log(totalGuessed);
+        if (totalGuessed === myRandomNumber) {
+          console.log("You Win!");
+          wins++;
+          $("#wins").html(wins);
+        } else if (totalGuessed > myRandomNumber) {
+          console.log("You Lost!");
+          losses++;
+          $("#losses").html(losses);
+        }
+      } else if (whichCrystal === "crystal-3") {
+        crystalValue3 = crystal3;
+        totalGuessed = totalGuessed + crystalValue3;
+        $("#totalGuessed").html(totalGuessed);
+        console.log(totalGuessed);
+        if (totalGuessed === myRandomNumber) {
+          console.log("You Win!");
+          wins++;
+          $("#wins").html(wins);
+        } else if (totalGuessed > myRandomNumber) {
+          console.log("You Lost!");
+          losses++;
+          $("#losses").html(losses);
+        }
+      } else if (whichCrystal === "crystal-4") {
+        crystalValue4 = crystal4;
+        totalGuessed = totalGuessed + crystalValue4;
+        $("#totalGuessed").html(totalGuessed);
+        console.log(totalGuessed);
+        if (totalGuessed === myRandomNumber) {
+          console.log("You Win!");
+          wins++;
+          $("#wins").html(wins);
+        } else if (totalGuessed > myRandomNumber) {
+          console.log("You Lost!");
+          losses++;
+          $("#losses").html(losses);
+        }
+      }
     }
   });
 });
