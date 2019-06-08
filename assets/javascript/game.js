e6611f37b6735382ac1dcfea4fe8509ce9f3484e
Martin Akram <martin.akram@gmail.com>
2019-06-07 22:31:15 -0400

    making more updates to jquery code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var wins = 0;
   var losses = 0;
   var totalGuessed = 0;
+  var targetNumber = $("#targetNumber");
 
   //This variable, "myRandomNumber" generates a random number that the user must match
   function generateRandomNumber() {
@@ -11,9 +12,6 @@ $(document).ready(function() {
   }
   generateRandomNumber();
 
-  var myRandomNumber = $("#targetNumber").html();
-  console.log(myRandomNumber);
-
   var crystal1 = Math.floor(Math.random() * 12) + 1;
   var crystal2 = Math.floor(Math.random() * 12) + 1;
   var crystal3 = Math.floor(Math.random() * 12) + 1;
@@ -21,23 +19,26 @@ $(document).ready(function() {
 
   function resetGame() {
     generateRandomNumber();
+    var myRandomNumber = $("#targetNumber").html();
     totalGuessed = 0;
     $("#totalGuessed").html(totalGuessed);
   }
 
   $(".crystal").on("click", function() {
     whichCrystal = $(this).attr("value");
-    if (totalGuessed < myRandomNumber) {
+    if (totalGuessed < targetNumber.html()) {
       if (whichCrystal === "crystal-1") {
         crystalValue1 = crystal1;
         totalGuessed = totalGuessed + crystalValue1;
+        console.log(totalGuessed);
+        console.log(targetNumber.html());
         $("#totalGuessed").html(totalGuessed);
-        if (totalGuessed == myRandomNumber) {
+        if (totalGuessed == targetNumber.html()) {
           wins++;
           $("#wins").html(wins);
           $("#message").html("Yay, You Won!");
           resetGame();
-        } else if (totalGuessed > myRandomNumber) {
+        } else if (totalGuessed > targetNumber.html()) {
           losses++;
           $("#losses").html(losses);
           $("#message").html("Oh No, You Lost!");
@@ -46,13 +47,15 @@ $(document).ready(function() {
       } else if (whichCrystal === "crystal-2") {
         crystalValue2 = crystal2;
         totalGuessed = totalGuessed + crystalValue2;
+        console.log(totalGuessed);
+        console.log(targetNumber.html());
         $("#totalGuessed").html(totalGuessed);
-        if (totalGuessed == myRandomNumber) {
+        if (totalGuessed == targetNumber.html()) {
           wins++;
           $("#wins").html(wins);
           $("#message").html("Yay, You Won!");
           resetGame();
-        } else if (totalGuessed > myRandomNumber) {
+        } else if (totalGuessed > targetNumber.html()) {
           losses++;
           $("#losses").html(losses);
           $("#message").html("Oh No, You Lost!");
@@ -61,13 +64,15 @@ $(document).ready(function() {
       } else if (whichCrystal === "crystal-3") {
         crystalValue3 = crystal3;
         totalGuessed = totalGuessed + crystalValue3;
+        console.log(totalGuessed);
+        console.log(targetNumber.html());
         $("#totalGuessed").html(totalGuessed);
-        if (totalGuessed == myRandomNumber) {
+        if (totalGuessed == targetNumber.html()) {
           wins++;
           $("#wins").html(wins);
           $("#message").html("Yay, You Won!");
           resetGame();
-        } else if (totalGuessed > myRandomNumber) {
+        } else if (totalGuessed > targetNumber.html()) {
           losses++;
           $("#losses").html(losses);
           $("#message").html("Oh No, You Lost!");
@@ -76,13 +81,15 @@ $(document).ready(function() {
       } else if (whichCrystal === "crystal-4") {
         crystalValue4 = crystal4;
         totalGuessed = totalGuessed + crystalValue4;
+        console.log(totalGuessed);
+        console.log(targetNumber.html());
         $("#totalGuessed").html(totalGuessed);
-        if (totalGuessed == myRandomNumber) {
+        if (totalGuessed == targetNumber.html()) {
           wins++;
           $("#wins").html(wins);
           $("#message").html("Yay, You Won!");
           resetGame();
-        } else if (totalGuessed > myRandomNumber) {
+        } else if (totalGuessed > targetNumber.html()) {
           losses++;
           $("#losses").html(losses);
           $("#message").html("Oh No, You Lost!");
